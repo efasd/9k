4c38b09e286af05fb69e4d5d8f312f5c08d9cc0f
xxxxibgdrgn <cs12d12@gmail.com>
2020-11-29 10:59:21 +0800

    fix: CORS added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
