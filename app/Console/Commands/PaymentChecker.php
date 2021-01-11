23887712e374e1671715578328f74070489f65d1
Bolorshagai <bolorshagai.e@novelsoft.mn>
2021-01-11 21:46:43 +0800

    fix: getEmployeeAppointment removed second string

--- app/Console/Commands/PaymentChecker.php
+++ app/Console/Commands/PaymentChecker.php
@@ -3,11 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Http\Controllers\API\payment\auth\PaymentAuthAPIController;
+use App\Notifications\NewOrder;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Date;
 use DateTime;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
 
 session_start();
 
@@ -112,10 +114,14 @@ class PaymentChecker extends Command
                 if (count($response->rows) > 0) {
                     if ($response->rows[0]->payment_status === 'PAID') {
                         $now = new DateTime('NOW');
-                        DB::table('invoice')
-                            ->where('id', $invoice->id)
-                            ->update(['accepted' => true, 'accept_date' => $now]);
+//                        DB::table('invoice')
+//                            ->where('id', $invoice->id)
+//                            ->update(['accepted' => true, 'accept_date' => $now]);
 
+                        $order = DB::table('orders')->find($invoice->order_id);
+                        $updated = DB::table('orders')->where('id', $invoice->order_id)->update(['order_status_id' => 5]);
+                        dd($order->productOrders[0]);
+                        Notification::send($order->productOrders[0]->product->market->users, new NewOrder($order));
                     }
                 }
             }
