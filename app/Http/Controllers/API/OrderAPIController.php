f548669f7c2591760c0cfffa32708f4cf1d2f68d
Bolorshagai <bolorshagai.e@novelsoft.mn>
2021-01-24 15:29:34 +0800

    fix: payment change to cash (only)

--- app/Http/Controllers/API/OrderAPIController.php
+++ app/Http/Controllers/API/OrderAPIController.php
@@ -191,47 +191,58 @@ class OrderAPIController extends Controller
                 );
                 $reData['lines'][0] = $line;
             }
-            try {
+            $urls = [];
+            $cash = (object)[
+                'name' => 'Бэлэнээр төрөл',
+                'description' => '',
+                'logo' => 'https://9000.mn/storage/app/public/282/conversions/cash-thumb.jpg',
+                'link' => ''
+            ];
+            array_push($urls, $cash);
+            $response->original['data']['urls'] = $urls;
 
-                $client = new Client();
-                $request = $client->request(
-                    'POST',
-                    env('PAYMENT_IP') . '/v2/invoice',
-                    [
-                        'headers' => [
-                            'Authorization' => 'Bearer ' . $_SESSION['qpay_access_token'],
-                            'Content-Type' => 'application/json'
-                        ],
-                        'body' => json_encode($reData)
-                    ]
-                );
-
-                if ($request->getStatusCode() === 200) {
-                    $invoiceRes = json_decode($request->getBody());
-                    $insertInvoice = DB::table('invoice')
-                        ->updateOrInsert(
-                            [
-                                'user_id' => $response->getData()->data->user->id,
-                                'order_id' => $response->getData()->data->id
-                            ],
-                            [
-                                'user_id' => $response->getData()->data->user->id,
-                                'order_id' => $response->getData()->data->id,
-                                'active' => true,
-                                'accepted' => false,
-                                'invoice_id' => $invoiceRes->invoice_id,
-                                'qr_text' => $invoiceRes->qr_text,
-                                'start_date' => new DateTime('NOW')
-                            ]
-                        );
-                    $response->original['data']['urls'] = $invoiceRes->urls;
-
-                    return $this->sendResponse($response, __('lang.saved_successfully', ['operator' => __('lang.order')]));
-                }
-                return $this->sendError('Зарлагын хүсэлт үүсгэж чадсангүй', 500);
-            } catch (RequestException $e) {
-                return $this->sendError($e->getMessage(), 500);
-            }
+            return $this->sendResponse($response, __('lang.saved_successfully', ['operator' => __('lang.order')]));
+//            try {
+//
+//                $client = new Client();
+//                $request = $client->request(
+//                    'POST',
+//                    env('PAYMENT_IP') . '/v2/invoice',
+//                    [
+//                        'headers' => [
+//                            'Authorization' => 'Bearer ' . $_SESSION['qpay_access_token'],
+//                            'Content-Type' => 'application/json'
+//                        ],
+//                        'body' => json_encode($reData)
+//                    ]
+//                );
+//
+//                if ($request->getStatusCode() === 200) {
+//                    $invoiceRes = json_decode($request->getBody());
+//                    $insertInvoice = DB::table('invoice')
+//                        ->updateOrInsert(
+//                            [
+//                                'user_id' => $response->getData()->data->user->id,
+//                                'order_id' => $response->getData()->data->id
+//                            ],
+//                            [
+//                                'user_id' => $response->getData()->data->user->id,
+//                                'order_id' => $response->getData()->data->id,
+//                                'active' => true,
+//                                'accepted' => false,
+//                                'invoice_id' => $invoiceRes->invoice_id,
+//                                'qr_text' => $invoiceRes->qr_text,
+//                                'start_date' => new DateTime('NOW')
+//                            ]
+//                        );
+//                    $response->original['data']['urls'] = $invoiceRes->urls;
+//
+//                    return $this->sendResponse($response, __('lang.saved_successfully', ['operator' => __('lang.order')]));
+//                }
+//                return $this->sendError('Зарлагын хүсэлт үүсгэж чадсангүй', 500);
+//            } catch (RequestException $e) {
+//                return $this->sendError($e->getMessage(), 500);
+//            }
         }
     }
 
